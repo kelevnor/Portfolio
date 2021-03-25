a50eb738f1a0190798a93a5a7e8aca85a7b4d983
Marios Sifalakis <Marios.Sifalakis@ibm.com>
2021-03-25 17:01:23 -0400

    LoginActivity added

---
+++ app/src/main/java/com/ibm/mobilefirst/myportfolio/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.ibm.mobilefirst.myportfolio.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
